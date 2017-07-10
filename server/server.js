e105ae64f215095d1c253f3b32f86b77fdfdcc31
zwp <zhuweipeng@kuaiqiangche.com>
2017-07-10 18:20:48 +0800

    移除server  代码

--- server/server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-const api = require('./api');
-const db = require('./db');
-
-app.use(api);
-app.listen(8989, function () {
-  console.log('Visit http://localhost:8989')
-});
