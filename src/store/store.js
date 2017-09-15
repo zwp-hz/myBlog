8a78e15565cf8d06c5b891c61912a79371f9e2a5
朱为鹏 <1453928106@qq.com>
2017-09-15 11:15:47 +0800

    更改接口域名地址

--- src/store/store.js
+++ src/store/store.js
@@ -4,7 +4,7 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 const state = {
-	APIHOST: window.location.href.indexOf("www") != -1 ? "http://106.14.157.57:8989/" : "http://localhost:8989/",
+	APIHOST: window.location.href.indexOf("www") != -1 ? "http://www.zhuweipeng.me:8989/" : "http://localhost:8989/",
   	IMGHOST: "http://omdiu1qhf.bkt.clouddn.com/"
 }
 
