81814e8aa82ac6f1553809fa9a076a94ec6ff4db
朱为鹏 <1453928106@qq.com>
2019-08-16 11:39:52 +0800

    代码测试

---
+++ pages/code-test/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="code-test">
+    <span @click="toApp">唤起app</span>
+  </div>
+</template>
+
+<script>
+export default {
+  head: {
+    title: '代码测试'
+  },
+  methods: {
+    toApp() {
+      location.href = 'xuelesy://net.xuele.xuelec2/awake'
+
+      setTimeout(function() {
+        location.href =
+          'https://sj.qq.com/myapp/detail.htm?apkName=net.xuele.xuelec2'
+      }, 1000)
+    }
+  }
+}
+</script>
